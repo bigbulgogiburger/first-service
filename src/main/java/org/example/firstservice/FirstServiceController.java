ba3dd6c3dc42765ee0e657f5698baf01358baa63
dhpyun <dhpyun@dbinc.co.kr>
2024-07-15 15:17:54 +0900

    simple changes

--- src/main/java/org/example/firstservice/FirstServiceController.java
+++ src/main/java/org/example/firstservice/FirstServiceController.java
@@ -1,6 +1,9 @@
 package org.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/first-service")
 @Slf4j
+@RequiredArgsConstructor
 public class FirstServiceController {
+
+
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String welcome(){
@@ -24,7 +31,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check(){
-        return "Hi, there this is first service";
+    public String check(HttpServletRequest request){
+        log.info("Server port = {}",request.getServerPort());
+        return String.format("Hi, there this is first service on Port %s", env.getProperty("local.server.port"));
     }
 }
